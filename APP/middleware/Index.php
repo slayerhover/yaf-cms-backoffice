10e8d9869eb2133b1689b40487300aee20657234
slayerhover <34752931@qq.com>
2017-12-28 20:25:36 +0800

    添加类中间件处理器

--- APP/middleware/Index.php
+++ APP/middleware/Index.php
@@ -1,5 +1,9 @@
-<?phpuse Illuminate\Database\Capsule\Manager as DB;class IndexMiddle {
-	public function handle($postData){			#ret(1, [], 'token error');
+<?php
+use Illuminate\Database\Capsule\Manager as DB;
+class IndexMiddle {
+
+	public function handle($postData){
+		#ret(1, [], 'token error');
 		
-	}	
-}
+	}	
+}
